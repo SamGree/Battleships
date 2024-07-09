5c7b4cf788303f4600e4ec40919a4750ef7edb2e
Sam Green <sam.green7777@yahoo.com>
2024-07-09 18:02:04 +0000

    Initialize game board and score tracking

--- run.py
+++ run.py
@@ -1,3 +1,20 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+from random import randint
+
+
+scores = {'computer': 0, 'player': 0}
+"""
+Initializes a dictionary to keep track of the scores
+for the computer and the player, both starting at 0.
+"""
+class Board:
+    """
+    Defines a class Board which will represent the game board.
+    """
+    def __init__(self, size, num_ships, name, type):
+        self.size = size
+        self.board =[[" " for _ in range(size)] for _ in range(size)]
+        self.num_ships = num_ships
+        self.name = name
+        self.type = type
+        self.guesses = []
+        self.ships = []
