03523560c4158f7bcff17afb6d9e46dae9b968d0
Sam Green <sam.green7777@yahoo.com>
2024-07-09 22:30:58 +0000

    Add random_point function to generate random points within the board size

--- run.py
+++ run.py
@@ -60,4 +60,10 @@ def add_ship(self, x, y):
         else:
             self.ships.append((x, y))
             if self.type == "player":
-                self.board[x][y] = "~"            
+                self.board[x][y] = "~"  
+
+def random_point(size):
+        """
+        Returns a random point on the board between 0 and size
+        """
+        return randint(0, size - 1)
