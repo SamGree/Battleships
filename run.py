ca7b34bddcb37dd2c0bc890b90338511a296911b
Sam Green <sam.green7777@yahoo.com>
2024-07-09 19:49:03 +0000

    rename json file

--- run.py
+++ run.py
@@ -2,15 +2,16 @@ from random import randint
 
 
 scores = {'computer': 0, 'player': 0}
-        """
-        Initializes a dictionary to keep track of the scores
-        for the computer and the player, both starting at 0.
-        """
+"""
+Initializes a dictionary to keep track of the scores
+for the computer and the player, both starting at 0.
+"""
+        
 class Board:
         """
         Defines a class Board which will represent the game board.
         """
-    def __init__(self, size, num_ships, name, type):
+def __init__(self, size, num_ships, name, type):
         self.size = size
         self.board =[[" " for _ in range(size)] for _ in range(size)]
         self.num_ships = num_ships
@@ -19,7 +20,7 @@ class Board:
         self.guesses = []
         self.ships = []
 
-    def print(self, hide_ships = False) :
+def print(self, hide_ships = False) :
         print(f"{self.name}'s board:")   
         print("   " + "   ".join(str(i) for i in range(self.size)))
         for idx, row in enumerate(self.board):
@@ -32,7 +33,7 @@ class Board:
         print("  " + "---|" * self.size)      
 
 
-    def guess(self, x, y):
+def guess(self, x, y):
         """
         Process a guess at coordinates (x, y).
         This method checks if the guessed coordinates hit a ship.
